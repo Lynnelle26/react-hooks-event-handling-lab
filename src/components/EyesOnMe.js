6626c6d67a8b2398ab9ddec464ab509385b2fdae
lynnelle <lynnellewanja37@mail.com>
2024-04-30 12:44:21 +0300

    initial commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from "react";
+function EyesOnMe () {
+    function HandleFocus() {
+        console.log("Good!");
+    }
+    function HandleBlur() {
+        console.log("Hey! Eyes on me!");
+    }
+    return(
+        <button onFocus={HandleFocus} onBlur={HandleBlur}>Eyes on me</button>
+    );
+
+}
+export default EyesOnMe;
